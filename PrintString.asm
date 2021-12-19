db4f16351c9b5c3dacf8745e6dd69383338148b2
Durjoy <da-durjoy@outlook.com>
2021-12-20 00:00:36 +0600

    Print a String

---
+++ PrintString.asm
@@ -0,0 +1,33 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG DB "I LOVE BANGLADESH I LOVE JAVA $";DB MEANS DEFINE BYTE 
+SMS DB "da-durjoy@outlook.com $"
+.CODE 
+MAIN PROC
+     
+     MOV AX,@DATA;INITIALIZIE DATA SEGMENT  
+     MOV DS,AX   
+   
+   
+     LEA DX,MSG;lODA EFFECTIVE ADDRESS
+     MOV AH,9
+     INT 21H
+    
+     MOV AH,2
+     MOV DL,0DH
+     INT 21H
+     MOV DL,0AH
+     INT 21H
+    
+     
+     LEA DX,SMS;lODA EFFECTIVE ADDRESS
+     MOV AH,9
+     INT 21H
+     
+     
+          
+     MOV AH,4CH
+     INT 21H
+    MAIN ENDP
+END MAIN

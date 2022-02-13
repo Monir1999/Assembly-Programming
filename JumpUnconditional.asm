70d324f8c570812094d73c7befdc9f3fedcc859b
Durjoy <da-durjoy@outlook.com>
2022-02-13 11:35:39 +0600

    Unconditional Jump

---
+++ JumpUnconditional.asm
@@ -0,0 +1,50 @@
+;Unconditional Jump
+.model small
+     .stack 100h
+     .data
+
+     .code
+         main proc
+           
+             mov ah, 1 
+             int 21h
+             mov bl, al
+             
+             mov ah,2
+             mov dl,0dh
+             int 21h
+             mov dl,0ah
+             int 21h
+             
+             mov ah,1 
+             int 21h
+             mov bh,al
+             
+             jmp level_x
+             
+             mov ah,2
+             mov dl,0dh
+             int 21h
+             
+             mov dl,0ah
+             int 21h
+             
+             mov ah,2
+             mov dl,bh
+             int 21h
+             
+             
+             level_x:
+             mov ah,2
+             mov dl,0dh
+             int 21h
+             
+             mov dl,0ah
+             int 21h
+             
+             mov ah,2
+             mov dl,bl
+             int 21h
+             
+             main endp
+         end main
